60d008c5a3808cdcfadd7aa07a47d5fe3446a889
mrabushyn <99904851+mrabushyn@users.noreply.github.com>
2022-08-15 14:50:00 +0300

    task - 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,20 @@
-
+const form = document.querySelector(".login-form");
+console.log(form);
+form.addEventListener("submit", handleSubmit);
+function handleSubmit(event) {
+    event.preventDefault();
+    
+    const {
+        elements: { email, password },
+    } = event.currentTarget;
+    
+    if (email.value === "" || password.value === "") {
+        alert("All fields must be filled");
+    }
+    
+    if (email.value !== "" && password.value !== "") {
+        const dataObj = `{${email.name} : ${email.value}, ${password.name} : ${password.value}}`;
+        console.log(dataObj);
+    }
+    event.currentTarget.reset();
+}
