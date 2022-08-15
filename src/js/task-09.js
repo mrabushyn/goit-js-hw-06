21a3499bcb9369188a7787179cacaeab19707830
mrabushyn <99904851+mrabushyn@users.noreply.github.com>
2022-08-15 15:53:05 +0300

    task -9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,11 @@
+const widgetEl = document.querySelector(".change-color");
+console.log(widgetEl);
+widgetEl.addEventListener("click", getRandomHexColor);
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    const hexColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    const textColor = document.querySelector("span.color");
+    textColor.textContent = hexColor;
+    const bodyColor = document.querySelector("body");
+    bodyColor.style.backgroundColor = hexColor;
 }
