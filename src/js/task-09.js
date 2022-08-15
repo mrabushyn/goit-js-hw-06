64c493e03ce07631d107b9b44032b511a378f73c
mrabushyn <99904851+mrabushyn@users.noreply.github.com>
2022-08-15 18:51:48 +0300

    task 10

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,11 +1,10 @@
 const widgetEl = document.querySelector(".change-color");
-console.log(widgetEl);
 widgetEl.addEventListener("click", getRandomHexColor);
 
 function getRandomHexColor() {
     const hexColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
     const textColor = document.querySelector("span.color");
     textColor.textContent = hexColor;
-    const bodyColor = document.querySelector("body");
-    bodyColor.style.backgroundColor = hexColor;
+    const bodyBgColor = document.querySelector("body");
+    bodyBgColor.style.backgroundColor = hexColor;
 }
--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,40 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+const hexColor = function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const inputEl = document.querySelector("input");
+const createBtn = document.querySelector("button[data-create]");
+const destroyBtn = document.querySelector("button[data-destroy]");
+const divBoxes = document.querySelector("#boxes");
+
+createBtn.addEventListener("click", (event) => {
+
+function createBoxes(amount) {
+  amount = inputEl.value;
+
+    const divArray = [];
+
+    let acc = 30;
+    for (let i = 1; i <= amount; i += 1) {
+        let divEl = document.createElement("div");
+        divEl.style.display = "inline-flex"
+        divEl.style.width = acc + 'px';
+        divEl.style.height = acc + "px";
+        divEl.style.paddingLeft = "1px";
+        divEl.textContent = `Box  ${i} ${hexColor()}`;
+        divEl.style.backgroundColor = hexColor();
+        divEl.style.fontSize = acc/4 + "px";
+        divEl.style.textAlign = "center";
+        acc += 10;
+        divArray.push(divEl);
+        divBoxes.append(...divArray);
+    }
+}
+
+createBoxes();
+
+destroyBtn.addEventListener("click", destroyBoxes);
+function destroyBoxes() {
+    divBoxes.innerHTML = "";
+}
+});
