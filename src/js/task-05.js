4925804f458a3ae47903eec1947b0e94abb5c0b7
mrabushyn <99904851+mrabushyn@users.noreply.github.com>
2022-08-16 20:29:25 +0300

    update task 6

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -1,10 +1,8 @@
 const outputValue = document.querySelector("span#name-output");
-console.log(outputValue);
-
+// console.log(outputValue);
 const outputText = outputValue.textContent;
-
 const inputValue = document.querySelector("input#name-input");
-console.log(inputValue.placeholder);
+// console.log(inputValue.placeholder);
 
 inputValue.addEventListener("input", (event) => {
     if (event) {
--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,24 +1,35 @@
 const inputEl = document.querySelector(`input#validation-input`);
-console.log(inputEl);
-let inputTextLengthValue = 0;
+const inputDataLength = inputEl.dataset.length;
 
 inputEl.addEventListener("input", (event) => {
+    const numberOfEnterSymbol = event.currentTarget.value.length;
     if (event) {
-        console.log(event.currentTarget.value.length);
-        let inputTextLength = event.currentTarget.value.length;
-        inputTextLengthValue = inputTextLength;
-    }
-});
-
-inputEl.addEventListener("blur", () => {
-    if (inputTextLengthValue !== 0 && inputTextLengthValue !== 6) {
-        inputEl.classList.add("invalid");
-    } if (inputTextLengthValue === 6) {
-        inputEl.classList.remove("invalid");
         inputEl.classList.add("valid");
-    } if (inputTextLengthValue === 0) {
-        inputEl.classList.remove("valid");
-        inputEl.classList.remove("invalid");
+        console.log(inputEl);
     }
+    inputDataLength != numberOfEnterSymbol
+        ? inputEl.classList.add("invalid")
+        : inputEl.classList.remove("invalid");
 });
 
+
+
+
+
+// if (numberOfEnterSymbol === inputDataLength) {
+//     inputEl.classList.replace("valid", "invalid");}
+
+// let inputTextLength = event.currentTarget.value.length;
+// inputTextLengthValue = inputTextLength;
+
+// inputEl.addEventListener("blur", () => {
+//     if (inputTextLengthValue !== 0 && inputTextLengthValue !== 6) {
+//         inputEl.classList.add("invalid");
+//     } if (inputTextLengthValue === 6) {
+//         inputEl.classList.remove("invalid");
+//
+//     } if (inputTextLengthValue === 0) {
+//         inputEl.classList.remove("valid");
+//         inputEl.classList.remove("invalid");
+//     }
+// });
