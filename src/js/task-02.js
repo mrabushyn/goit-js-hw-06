51462825e06b465d3267d450c8291f1e98cece1c
mrabushyn <99904851+mrabushyn@users.noreply.github.com>
2022-08-16 18:58:39 +0300

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,18 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+    "Potatoes",
+    "Mushrooms",
+    "Garlic",
+    "Tomatos",
+    "Herbs",
+    "Condiments",
 ];
-
-  ingredients.map((ingredient) => {
-      const list = document.querySelector("#ingredients");
-      const listItem = document.createElement("li");
-      listItem.classList.add("item");
-      listItem.textContent = ingredient;
-      list.append(listItem);
-      console.log(listItem);
-  }
-  )
+const list = document.querySelector("#ingredients");
+const ingredientsList = [];
+ingredients.map((ingredient) => {
+        const listItem = document.createElement("li");
+        listItem.classList.add("item");
+        listItem.textContent = ingredient;
+        ingredientsList.push(listItem.outerHTML);
+    })
+list.insertAdjacentHTML("beforeend", ingredientsList.join(""));
