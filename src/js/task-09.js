a6ba6db407d4d7f64c3ef005950638ec0d9b0d3b
mrabushyn <99904851+mrabushyn@users.noreply.github.com>
2022-08-17 19:29:49 +0300

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,10 +1,13 @@
 const widgetEl = document.querySelector(".change-color");
 widgetEl.addEventListener("click", getRandomHexColor);
+    
+const textColor = document.querySelector("span.color");
+    const bodyBgColor = document.querySelector("body");
 
 function getRandomHexColor() {
     const hexColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-    const textColor = document.querySelector("span.color");
+    // const textColor = document.querySelector("span.color");
     textColor.textContent = hexColor;
-    const bodyBgColor = document.querySelector("body");
+    // const bodyBgColor = document.querySelector("body");
     bodyBgColor.style.backgroundColor = hexColor;
 }
